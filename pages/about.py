cde99bd64fe1fdad30607cfe29dcfdd311266deb
Toni <inotives@gmail.com>
2024-09-11 19:24:23 +0800

    added career timeline to about me page

--- pages/about.py
+++ pages/about.py
@@ -1,16 +1,22 @@
 import streamlit as st
+import streamlit_timeline as sl
 
 def app():
     st.session_state.page = 'about'
+    st.subheader("About me")
     st.markdown(
         """
-        # About me 
-
         I began my career as a web developer, proficient in PHP (Laravel), JavaScript (React, Node.js), and Python (Flask). My interest in cryptocurrencies led me to join ItBit, a prominent cryptocurrency exchange, in 2015.
 
-        During my time at ItBit, I gained hands-on experience with various aspects of cryptocurrency technology, including wallet key management, blockchain investigation, and developing Python tools for exchange API testing.
+        During my time at ItBit (Paxos), I gained hands-on experience with various aspects of cryptocurrency technology, including wallet key management, blockchain investigation, and developing Python scripts for data ingestion and manipulations. 
 
-        Leveraging my technical skills and passion for data, I transitioned into a data analyst role. This allowed me to combine my expertise in web development with my interest in cryptocurrencies, enabling me to contribute significantly to data-driven decision-making in the crypto space.
+        Leveraging my technical skills and passion for data, I transitioned into a data analyst role. This allowed me to combine my expertise in web development with my interest in cryptocurrencies, enabling me to contribute significantly to data-driven decision-making in ever changing crypto space.
         """
     )
     
+    st.subheader('Career Timeline')
+
+    with st.spinner(text="Career Timeline"):
+        with open('timeline.json', "r") as f:
+            data = f.read()
+            sl.timeline(data, height=600)
--- pages/home.py
+++ pages/home.py
@@ -6,9 +6,19 @@ def app():
     st.caption("Welcome to inoTives Showcase!")
     st.markdown(
         """
-        Hi my name is Toni (@inotives). I am a data enthusiat. This streamlit webapps was meant for experiment and showcase some data projects i worked on. 
-        It is also record for my journey in the field of data science. 
+        Hi, I'm Toni ([@inotives](https://www.linkedin.com/in/inotives/)), a passionate data enthusiast. This Streamlit web app serves as a showcase for my data science projects and a living record of my journey in this exciting field.
+
+        My goal is to explore the vast potential of data analysis in various domains, including:
+        - **Cryptocurrency**: Delving into market trends, sentiment analysis, and forecasting.
+        - **Macroeconomics**: Examining economic indicators and their impact on financial markets and correlation to cryto markets
+        - **Data exploration and visualization**: Experimenting with different techniques to uncover insights and trends.
+        - **Machine learning**: Building and deploying machine learning models for various tasks.
+        - **LLMs and generative AI**: Exploring the capabilities and applications of these cutting-edge technologies.
+
+        This web app also serves as a personal journal, documenting my experiences, learnings, and challenges in the world of data science and AI. 
+        I invite you to explore my projects, provide feedback, and join me on this exciting journey.
         """
     )
+
 
     
